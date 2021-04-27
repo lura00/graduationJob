4a723dc82af9270c50d846a06cc3ea5592010dfd
Sondre Kulseng <55538572+sondrekulseng@users.noreply.github.com>
2021-04-27 23:26:14 +0200

    db update

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,16 @@
-CREATE DATABASE test;
-CREATE USER 'sondre'@'localhost' IDENTIFIED BY '';
-GRANT ALL ON test.* TO 'sondre'@'localhost';
-USE test;
+CREATE DATABASE webshop;
+CREATE USER 'sondre'@'localhost' IDENTIFIED BY 'pass';
+GRANT ALL ON webshop.* TO 'sondre'@'localhost';
+USE webshop;
 
-CREATE TABLE vare (
-  id int PRIMARY KEY,
-  navn varchar(50) NOT NULL
-)
+CREATE TABLE product (
+  productID int PRIMARY KEY auto_increment,
+  name varchar(50) NOT NULL,
+  description varchar(100) NOT NULL,
+  price float,
+  img varchar(50) NOT NULL
+);
+
+INSERT INTO product (name, description, price, img) VALUES ("Test 1", "description", 100.0, "");
+INSERT INTO product (name, description, price, img) VALUES ("Test 2", "description", 200.0, "");
+INSERT INTO product (name, description, price, img) VALUES ("Test 2", "description", 300.0, "");
--- flask/demo.py
+++ flask/demo.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_mysqldb import MySQL
 app = Flask(__name__, template_folder='static/')
+
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'sondre'
+app.config['MYSQL_PASSWORD'] = 'pass'
+app.config['MYSQL_DB'] = 'webshop'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+@app.route('/api/search', methods = ['GET', 'POST'])
+def search():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM product")
+    myresult = cur.fetchall()
+    print(myresult)
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port = 5001, debug = True)
